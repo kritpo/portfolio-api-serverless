0d2ad8aa2895a04782d180b4611cd153f80dc3bc
Jimmy <jimmy.weng@efrei.net>
2021-03-01 10:28:22 +0100

    refactor: change return format and params name

--- src/infra/resumes/resumeGET.js
+++ src/infra/resumes/resumeGET.js
@@ -27,9 +27,9 @@ const resumeGET = async event => {
 		);
 
 		// hydrate the resume
-		response = resume.hydrate(ddb.dbReadByIdAndFilter).then(() => {
+		response = resume.hydrate(ddb.dbReadByIdAndFilter).then(() =>
 			// if success, return ok response with the resume
-			return http.ok({
+			http.ok({
 				username: resume.username,
 				languageCode: resume.languageCode,
 				basics: resume.basics,
@@ -41,71 +41,71 @@ const resumeGET = async event => {
 				languages: resume.languages,
 				interests: resume.interests,
 				references: resume.references
-			});
-		});
+			})
+		);
 	}
 
-	response = response
-		// catch eventual errors when the resume is hydrated with the language query
-		.catch(err => {
-			// check if the error is a `not found` error
-			if (err instanceof errors.NotFoundError) {
-				// return nothing, to pursue the stack, will try to find the default resume
-				return;
-			}
+	return (
+		response
+			// catch eventual errors when the resume is hydrated with the language query
+			.catch(err => {
+				// check if the error is a `not found` error
+				if (err instanceof errors.NotFoundError) {
+					// return nothing, to pursue the stack, will try to find the default resume
+					return;
+				}
 
-			// rethrow the error to be catch by the last catcher
-			throw err;
-		})
-		.then(data => {
-			// check if data is defined
-			if (data !== undefined) {
-				// return directly the result as it was already computed
-				return data;
-			}
+				// rethrow the error to be catch by the last catcher
+				throw err;
+			})
+			.then(response => {
+				// check if the response is defined
+				if (response !== undefined) {
+					// return directly the response as it was already computed
+					return response;
+				}
 
-			// generate a new resume languages container
-			const resumeLang = new ResumeLang(input.params.username);
+				// generate a new resume languages container
+				const resumeLang = new ResumeLang(input.params.username);
 
-			// hydrate the resume languages container
-			return resumeLang.hydrate(ddb.dbReadByIdAndFilter).then(() => {
-				// generate a new resume
-				const resume = new Resume(
-					input.params.username,
-					resumeLang.defaultLanguage.languageCode
-				);
+				// hydrate the resume languages container
+				return resumeLang.hydrate(ddb.dbReadByIdAndFilter).then(() => {
+					// generate a new resume
+					const resume = new Resume(
+						input.params.username,
+						resumeLang.defaultLanguage.languageCode
+					);
 
-				// return the hydrated resume
-				return resume.hydrate(ddb.dbReadByIdAndFilter).then(() => {
-					return http.ok({
-						username: resume.username,
-						languageCode: resume.languageCode,
-						basics: resume.basics,
-						work: resume.work,
-						volunteer: resume.volunteer,
-						education: resume.education,
-						projects: resume.projects,
-						skills: resume.skills,
-						languages: resume.languages,
-						interests: resume.interests,
-						references: resume.references
-					});
+					// return the hydrated resume
+					return resume.hydrate(ddb.dbReadByIdAndFilter).then(() =>
+						http.ok({
+							username: resume.username,
+							languageCode: resume.languageCode,
+							basics: resume.basics,
+							work: resume.work,
+							volunteer: resume.volunteer,
+							education: resume.education,
+							projects: resume.projects,
+							skills: resume.skills,
+							languages: resume.languages,
+							interests: resume.interests,
+							references: resume.references
+						})
+					);
 				});
-			});
-		})
-		.catch(err => {
-			// log the error
-			console.error(`ERROR: ${err}`);
+			})
+			.catch(err => {
+				// log the error
+				console.error(`ERROR: ${err}`);
 
-			// check the type of error
-			if (err instanceof errors.NotFoundError) {
-				return http.notFound();
-			}
+				// check the type of error
+				if (err instanceof errors.NotFoundError) {
+					return http.notFound();
+				}
 
-			return http.internalServerError();
-		});
-
-	return response;
+				return http.internalServerError();
+			})
+	);
 };
 
 module.exports = resumeGET;
