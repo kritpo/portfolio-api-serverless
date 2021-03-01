4e927ff0a88764ebe418b64e3b57e01abcf41d3c
Jimmy <jimmy.weng@efrei.net>
2021-03-01 13:51:52 +0100

    fix: add dbUpdateByExpression in ddb modules

--- src/io/ddb-io/index.js
+++ src/io/ddb-io/index.js
@@ -4,11 +4,13 @@
 const dbCreate = require('./dbCreate');
 const dbReadByIdAndFilter = require('./dbReadByIdAndFilter');
 const dbUpdateByReplace = require('./dbUpdateByReplace');
+const dbUpdateByExpression = require('./dbUpdateByExpression');
 const dbDelete = require('./dbDelete');
 
 module.exports = {
 	dbCreate,
 	dbReadByIdAndFilter,
 	dbUpdateByReplace,
+	dbUpdateByExpression,
 	dbDelete
 };
